8980afa12203db98acae6e3f737c96b3517a6564
arianaburga <56941154+arianaburga@users.noreply.github.com>
2019-10-31 20:22:24 -0600

    Add files via upload

---
+++ burga1.py
@@ -0,0 +1,327 @@
+#calculadora nro1
+#esta calculadora realiza el calculo de la velocidad
+
+#declaracion de variables
+distancia,tiempo,velocidad=0.0,0.0,0.0
+
+#calculadora
+distancia=30
+tiempo=6
+velocidad=str(distancia/tiempo)
+
+#mostrar datos
+print("distancia = ",distancia)
+print("tiempo = ",tiempo)
+print("velocidad = ", velocidad)
+
+#calculadora nro2
+#esta calculadora realiza el calculo de la fuerza
+
+#declaracion de variables
+masa,aceleracion,fuerza=0.0,0.0,0.0
+
+#calculadora
+masa=39
+aceleracion=9.8
+fuerza=str(masa*aceleracion)
+
+#mostrar datos
+print("masa= ",masa)
+print("aceleracion= ",aceleracion)
+print("fuerza= ",fuerza)
+
+#calculadora nro3
+#esta calculadora realiza el calculo de un trapecio
+
+#delaracion de variables
+base_mayor,base_menor,altura=0.0,0.0,0.0
+
+#calculadora
+base_mayor=38
+base_menor=18
+altura=8
+area_del_trapecio= ((base_mayor+base_menor)*altura)/2
+#mostrar datos
+print("base mayor= ", base_mayor)
+print("base menor= ", base_menor)
+print("area_del_trapecio= ", area_del_trapecio)
+
+#calculadora nro3
+#esta calculadora realiza el calculo de la densidad
+
+#declaracion de variables
+masa,volumen,densidad=0.0,0.0,0.0
+
+#calculadora
+masa=87.2
+volumen=6
+densidad=(masa/volumen)
+#mostrar datos
+print("masa= ",masa)
+print("volumen= ",volumen)
+print("densidad= ",densidad)
+
+#calculadora nro4
+#esta calculadora realiza el calculo de la fuerza final
+
+#declaracion de variables
+velocidad_inicial,velocidad_final,aceleracion,tiempo=0.0,0.0,0.0,0.0
+
+#calculadora
+velocidad_inicial=56
+aceleracion=9.8
+tiempo=4
+velocidad_final=(velocidad_final+(aceleracion*tiempo))
+#mostrar datos
+print("velocidad_inical= ",velocidad_inicial)
+print("aceleracion= ",aceleracion)
+print("velocida_final= ",velocidad_final)
+
+#calculadora nro5
+#esta calculadora realiza el calculo de velocidad tangecial
+
+#delaracion de variables
+velocidad_angular,radio,velocidad_tangencial=0.0,0.0,0.0
+
+#calculadora
+velocidad_angular=25.4
+radio=2
+velocidad_tangencial=(velocidad_angular*radio)
+#mostrar datos
+print("velocida angular= ",velocidad_angular)
+print("radio= ",radio)
+print("velocidad tangencial= ",velocidad_tangencial)
+
+#calcuadora nro6
+#esta calculadora realiza el calculo de la potencia
+
+#declaracion de variables
+fuerza,velocidad,potencia=0.0,0.0,0.0
+
+#calculadora
+fuerza=237
+velocidad=16
+potencia=(fuerza*velocidad)
+#mostrar datos
+print("fuerza= ",fuerza)
+print("velocidad= ",velocidad)
+print("potencia= ",potencia)
+
+#calculadora nro7
+#esta calculadora realiza el calculo del trabajo
+
+#declaracion de variables
+fuerza,distancia,trabajo=0.0,0.0,0.0
+
+#calculadora
+fuerza=56.3
+distancia=5
+trabajo=(fuerza*distancia)
+#mostrar datos
+print("fuerza= ",fuerza)
+print("distancia= ",distancia)
+print("trabajo= ",trabajo)
+
+#calculadora nro8
+#esta calculadora realiza calculo de energia mecanica
+
+#declaracion de datos
+enegia_cinetica,energia_potencial,energia_mecanica=0.0,0.0,0.0
+
+#calculadora
+energia_cinetica=984
+energia_potencial=283
+energia_mecanica=(energia_cinetica+energia_potencial)
+#mostrar datos
+print("energia cinetica= ",energia_cinetica)
+print("energia potencial= ",energia_potencial)
+print("energia mecanica= ",energia_mecanica)
+
+#calculadora nro9
+#esta calculadora realiza calculo de la carga electrica
+
+#declaracion de datos
+electrones,nro_de_electrones,carga_electrica=0.0,0.0,0.0
+
+#calculadora
+electrones=1.6
+nro_de_electrones=15
+carga_electrica=(electrones*nro_de_electrones)
+#mostrar datos
+print("electrones= ",electrones)
+print("numero de electrones= ",nro_de_electrones)
+print("carga electrica= ",carga_electrica)
+
+#calculadora nro10
+#esta calculadora realiza calculo del campo electrico
+
+#declaracion de datos
+fuerza_electrica,carga_electrica,campo_electrico=0.0,0.0,0.0
+
+#calculadora
+fuerza_electrica=85
+carga_electrica=3
+campo_electrico=(fuerza_electrica/carga_electrica)
+#mostrar datos
+print("fuerza electrica= ",fuerza_electrica)
+print("carga electrica= ", carga_electrica)
+print("campo electrico= ",campo_electrico)
+
+#calculadora nro11
+#esta calculadora raliza calculo del calor
+
+#declaracion de datos
+trabajo,variacion_de_energia,calor=0.0,0.0,0.0
+
+#calculadora
+trabajo=382
+variacion_de_energia=53
+calor=(trabajo+variacion_de_energia)
+#mostrar datos
+print("trabajo= ",trabajo)
+print("variacion de energia= ",variacion_de_energia)
+print("calor= ",calor)
+
+#calculadora nro12
+#esta calculadora realiza calculo de la potencia entregada
+
+#declaracion de datos
+potencia_util,potencia_perdida,potencia_entregada=0.0,0.0,0.0
+
+#calculadora
+potencia_util=234.3
+potencia_perdida=24.6
+potencia_entregada=(potencia_util+potencia_perdida)
+#mostrar datos
+print("potenia util= ",potencia_util)
+print("potencia perdida= ",potencia_perdida)
+print("potencia entregada= ",potencia_entregada)
+
+#calculadora nro13
+#esta calculadora realiza calculo de energia potencial gravitatoria
+
+#declaracion de datos
+masa,gravedad,altura,energia_potencial_gravitatoria=0.0,0.0,0.0,0.0
+
+#calculadora
+masa=93
+gravedad=9.8
+altura=32
+energia_potencial_gravitatoria=((masa*gravedad)*altura)
+#mostrar datos
+print("masa= ", masa)
+print("gravedad= ",gravedad)
+print("altura= ",altura)
+print("energia potencial= ",energia_potencial)
+
+#calculadora nro14
+#esta clculadora realiza calculo de la presion hidrostatica
+
+#declaracion de datos
+densidad_liquido,gravedad,altura,presion_hidrostatica=0.0,0.0,0.0,0.0
+
+#calculadora
+densidad_liquido=13.9
+gravedad=9.8
+altura=6
+presion_hidrostatica=((densidad_liquido*gravedad)*altura)
+#mostrar datos
+print("densidad del liquido= ",densidad_liquido)
+print("gravedad= ",gravedad)
+print("altura= ",altura)
+print("presion hidrostatica= ",presion_hidrostatica)
+
+#calculadora nro15
+#esta calculadora realiza calculo del peso especifico
+
+#declaracion de datos
+peso,volumen,peso_espesifico=0.0,0.0,0.0
+
+#calculadora
+peso=45
+volumen=12
+peso_espesifico=(peso/volumen)
+#mostrar datos
+print("peso= ",peso)
+print("volumen= ",volumen)
+print("peso especifico= ",peso_espesifico)
+
+#calculadora nro16
+#esta calculadora realiza calculo de la presion
+
+#declaracion de datos
+fuerza,area,presion=0.0,0.0,0.0
+
+#calculadora
+fuerza=48
+area=24
+presion=(fuerza/area)
+#mostrar datos
+print("fuerza= ",fuerza)
+print("area= ",area)
+print("presion= ",presion)
+
+#calculadora nro17
+#esta calculadora realiza calculo de la longitud de onda
+
+#declaracion de datos
+velocidad_de_la_luz,fuerza,longitud_de_onda=0.0,0.0,0.0
+
+#calculadora
+velocidad_de_la_luz=3.10
+fuerza=24
+longitud_de_onda=(velocidad/fuerza)
+#mostrar datos
+print("velocidad de la luz= ",velocidad_de_la_luz)
+print("fuerza= ",fuerza)
+print("longitud de onda= ",longitud_de_onda)
+
+#calculadora nro18
+#esta calculadora realiza calculo de la sumatoria de una serie
+
+#declaracion de datos
+primer_sumando,ultimo_sumando,nro_de_sumandos,sumatoria_serie=0.0,0.0,0.0,0.0
+
+#calculadora
+primer_sumando=2
+ultimo_sumando=38
+nro_de_sumandos=10
+sumatoria_serie=((primer_sumando+ultimo_sumando)*nro_de_sumandos)/2
+#mostrar datos
+print("primer sumando= ",primer_sumando)
+print("ultimo sumando= ",ultimo_sumando)
+print("numero de sumandos= ",nro_de_sumandos)
+print("sumatoria de la serie= ",sumatoria_serie)
+
+#calculadora nro19
+#esta calculadora realiza calculo de la intensidad de la corriente electrica
+
+#declaracion de datos
+carga,tiempo,intensidad_de_corriente=0.0,0.0,0.0
+#calculadora
+carga=9
+tiempo=2
+intensidad_de_corriente=(carga/tiempo)
+#mostrar datos
+print("carga= ",carga)
+print("tiempo= ",tiempo)
+print("intensidad de correinte electrica= ",intensidad_de_corriente)
+
+#calculadora nro20
+#esta calculadora realiza calculo del tiempo de encuentro
+
+#declaracion de datos
+distancia,velocidad_inicial,velocidad_final,tiempo_de_encuentro=0.0,0.0,0.0,0.0
+#calculadora
+distancia=98
+velocidad_inicial=24
+velocidad_final=54
+tiempo_de_encuentro=(distancia/(velocidad_inicial+velocidad_final))
+#mostrar datos
+print("distancia= ",distancia)
+print(" velocidad inicial= ",velocidad_inicial)
+print("velocidad final=",velocidad_final)
+print("tiempo de encuentro= ",tiempo_de_encuentro)
+
+
+
---
+++ burga4.py
@@ -0,0 +1,14 @@
+#calculadora nro4
+#esta calculadora realiza el calculo de la densidad
+
+#declaracion de variables
+masa,volumen,densidad=0.0,0.0,0.0
+
+#calculadora
+masa=87.2
+volumen=6
+densidad=(masa/volumen)
+#mostrar datos
+print("masa= ",masa)
+print("volumen= ",volumen)
+print("densidad= ",densidad)
